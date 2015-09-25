396bd98ae47fc67ae4be5a14f0136b3003686503
Dustin Zeisler <dustin@zeisler.net>
2015-09-25 11:21:18 -0700

    Create unified API to access association, attributes, and other global model data.

--- lib/active_record_schema_scrapper.rb
+++ lib/active_record_schema_scrapper.rb
@@ -1,6 +1,34 @@
 require "active_record_schema_scrapper/version"
-require 'active_record_schema_scrapper/attribute'
+require "active_record_schema_scrapper/attribute"
+require "active_record_schema_scrapper/attributes"
+require "active_record_schema_scrapper/association"
+require "active_record_schema_scrapper/associations"
 
 module ActiveRecordSchemaScrapper
 
+  def self.new(model:)
+    MetaSchema.new(model)
+  end
+
+  class MetaSchema
+
+    def initialize(model)
+      @model = model
+    end
+
+    def associations(**args)
+      Associations.new(args.merge({model: model}))
+    end
+
+    def attributes(**args)
+      Attributes.new(args.merge({model: model}))
+    end
+
+    def table_name
+      model.table_name
+    end
+
+    private
+    attr_reader :model
+  end
 end
--- spec/active_record_schema_scrapper_spec.rb
+++ spec/active_record_schema_scrapper_spec.rb
@@ -1,5 +1,36 @@
-require 'spec_helper'
+require "spec_helper"
+require "models/db"
+require "models"
 
 describe ActiveRecordSchemaScrapper do
+  describe "new" do
 
+    subject { described_class.new(model: User) }
+
+    describe "#attributes" do
+      it "passes model in" do
+        expect(described_class::Attributes).to receive(:new).and_call_original.with({model: User})
+        subject.attributes
+      end
+
+      it "returns an Attributes class" do
+        expect(subject.attributes.class).to eq(described_class::Attributes)
+      end
+    end
+
+    describe "#associations" do
+      it "passes any attributes through" do
+        expect(described_class::Associations).to receive(:new).and_call_original.with({model: User, types: [:belongs_to]})
+        subject.associations(types: [:belongs_to])
+      end
+
+      it "returns an Associations class" do
+        expect(subject.associations.class).to eq(described_class::Associations)
+      end
+    end
+
+    describe "table_name" do
+      it { expect(subject.table_name).to eq('users') }
+    end
+  end
 end
